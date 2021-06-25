e8ecf224d07a343a12f961918157d03163be7c5b
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-25 16:59:09 -0400

    fixed broken link to BookDetail page

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Book = (props) => {
   return (
     <>
-      <Link to={`/book/${props.id}`}>
+      <Link to={`/books/${props.id}`}>
         <img src={props.imgURL} alt={props.title} />
       </Link>
     </>
