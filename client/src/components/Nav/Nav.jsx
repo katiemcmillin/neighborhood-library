e0c113b74258651613fd9815bcda8e9f626308a1
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-24 21:44:12 -0400

    added Nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,48 +1,32 @@
 // import "./Nav.css";
-// import { NavLink } from "react-router-dom";
-// import logo from "../../images/logo.png"
+import { Link } from "react-router-dom";
+import logo from "../../images/logo.png";
 
-// const authenticatedOptions = (
-//   <>
-//     <NavLink className="link" to="/add-book">
-//       Add Book
-//     </NavLink>
-//     <NavLink className="link" to="/sign-out">
-//       Logout
-//     </NavLink>
-//   </>
-// );
-// const unauthenticatedOptions = (
-//   <>
-//     <NavLink className="link" to="/sign-in">
-//       Login
-//     </NavLink>
-//     <NavLink className="link" to="/sign-up">
-//       Register
-//     </NavLink>
-//   </>
-// );
-// const alwaysOptions = (
-//   <>
-//     <NavLink className="link" to="/books">
-//       Books
-//     </NavLink>
-//   </>
-// );
-// const Nav = ({ user }) => {
-//   return (
-//     <nav>
-//       <div className="nav">
-//         <NavLink className="logo" to="/">
-//           <img src={logo} alt="Neighborhood Library"/>
-//         </NavLink>
-//         <div className="links">
-//           {user && <div className="link welcome">Hello, {user.username}</div>}
-//           {alwaysOptions}
-//           {user ? authenticatedOptions : unauthenticatedOptions}
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-// export default Nav;
+function Nav(props) {
+  const { currentUser, handleLogout } = props;
+  return (
+    <header>
+      <Link to="/">
+        <img src={logo} alt="Neighborhood Library" />
+      </Link>
+      {currentUser && (
+        <Link>
+          <p>Add Book</p>
+        </Link>
+      )}
+      {currentUser ? (
+        <p>Hello, {currentUser.username}!</p>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+      {currentUser ? (
+        <p onClick={handleLogout}>Logout</p>
+      ) : (
+        <Link to="/register">Register</Link>
+      )}
+      <hr />
+    </header>
+  );
+}
+
+export default Nav;
--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,15 +1,10 @@
-// import Nav from '../components/Nav/Nav'
-import logo from "../images/logo.png"
+import Nav from '../components/Nav/Nav'
+
 import {Link} from "react-router-dom"
 const Layout = (props) => {
   const { currentUser, children, handleLogout } = props
   return  (<div className='layout'>
-    <header><Link to='/'><img src={logo} alt='Neighborhood Library'/></Link>
-      {currentUser && <Link><p>Add Book</p></Link>}
-      {currentUser ? (<p>Hello, {currentUser.username}!</p>) : (<Link to='/login'>Login</Link>)}
-      {currentUser ? <p onClick={handleLogout }>Logout</p>: (<Link to='/register'>Register</Link>)}
-      <hr />
-    </header>
+    <Nav currentUser={currentUser} handleLogout={handleLogout}/>
         <div className="layout-children">
             {children}
         </div>
