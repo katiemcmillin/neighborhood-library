561ec89bbe0512f311871acabfabb4877ac1b34f
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-26 21:05:57 -0400

    cleaned up broken code

--- client/src/components/Rating/RatingCreate.jsx
+++ client/src/components/Rating/RatingCreate.jsx
@@ -6,21 +6,21 @@ import "./RatingCreate.css";
 import { getAllRatings, postRating } from "../../services/ratings";
 
 function RatingCreate(props) {
-  const [ratings, setRatings] = useState([])
+  const [ratings, setRatings] = useState([]);
   const [rating, setRating] = useState({
     number_of_stars: Number(""),
-    book_id: props.book.id  
+    book_id: props.book.id,
   });
   useEffect(() => {
     const fetchRatings = async () => {
       const ratingsData = await getAllRatings();
-      setRatings(ratingsData)
+      setRatings(ratingsData);
     };
     fetchRatings();
   }, []);
 
   const [hover, setHover] = useState(null);
-  
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setRating({
@@ -31,48 +31,62 @@ function RatingCreate(props) {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    let created = false
-    ratings.forEach(rating => {
-      if ((rating.user_id === props.currentUser.id) && (rating.book_id === rating.book_id)) {
-        console.log(rating.user_id, props.currentUser.id, rating.book_id, rating.book_id)
-        created = true
-        setRating({number_of_stars: rating.number_of_stars})
+    let created = false;
+    ratings.forEach((rating) => {
+      if (
+        rating.user_id === props.currentUser.id &&
+        rating.book_id === rating.book_id
+      ) {
+        console.log(
+          rating.user_id,
+          props.currentUser.id,
+          rating.book_id,
+          rating.book_id
+        );
+        created = true;
+        setRating({ number_of_stars: rating.number_of_stars });
       }
-    })
+    });
     if (!created) {
       const newRating = await postRating(rating);
-      console.log(newRating)
-    } 
-  
+      console.log(newRating);
+    }
   };
+  // let ratingValue = null;
+  // let userRating = null;
+  // ratings.forEach((rating) => {
+
+  //   if (rating.user_id === props.currentUser.id && rating.book_id === rating.book_id) {
+  //     rating = userRating
+  //     return true
+  //   }
+  // });
+
   return (
     <div className="rating-container">
       <form onSubmit={handleSubmit}>
-      {[...Array(5)].map((star, index) => {
-        const ratingValue = index + 1;
-        return (
-          
-          <label key={index }>
-            <input
-              type="radio"
-              name="number_of_stars"
-              value={ratingValue}
-              onClick={handleChange}
-            />
-            <FaStar
-              className="star"
-              onMouseEnter={() => setHover(ratingValue)}
-              onMouseLeave={() => setHover(null)}
-              color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
-              size={30}
-            />
+        {[...Array(5)].map((star, index) => {
+          const ratingValue = index + 1;
+          return (
+            <label key={index}>
+              <input
+                type="radio"
+                name="number_of_stars"
+                value={ratingValue}
+                onClick={handleChange}
+              />
+              <FaStar
+                className="star"
+                onMouseEnter={() => setHover(ratingValue)}
+                onMouseLeave={() => setHover(null)}
+                color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                size={30}
+              />
             </label>
-            
-            
-        );
-      })}
-        <button type='submit'>Save</button>
-        </form>
+          );
+        })}
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
