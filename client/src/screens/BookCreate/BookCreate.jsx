d0fa0ebbe8408c8e6400a6a63192739345c17dce
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-28 12:47:48 -0400

    fixed broken code

--- client/src/screens/BookCreate/BookCreate.jsx
+++ client/src/screens/BookCreate/BookCreate.jsx
@@ -1,3 +1,4 @@
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_responsive_form/
 import { useState } from "react";
 import { postBook, getAllBooks } from "../../services/books";
 import { Redirect } from "react-router-dom";
@@ -30,57 +31,66 @@ const BookCreate = (props) => {
     return <Redirect to="/" />;
   }
   return (
-      <form className="create-form" onSubmit={handleSubmit}>
-
-          <label>
-            Title:
+    <div className="container">
+      <form onSubmit={handleSubmit} action="action_page.php">
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="fname">Title:</label>
+          </div>
+          <div className="col-75">
             <input
-              className="input-title"
               type="text"
               name="title"
               value={book.title}
               onChange={handleChange}
             />
-          </label>
-    
-  
-          <label>
-            Author:
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="author">Author:</label>
+          </div>
+          <div className="col-75">
             <input
-              className="input-author"
               type="text"
               name="author"
               value={book.author}
               onChange={handleChange}
             />
-          </label>
-
-          <label>
-            Description:
-            <textarea
-              className="textarea-description"
-              type="text"
-              rows={10}
-              cols={25}
-              name="description"
-              value={book.description}
-              onChange={handleChange}
-            />
-          </label>
-
-          <label>
-            Image Link:
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="img_url">Image Link:</label>
+          </div>
+          <div className="col-75">
             <input
-              className="input-image-link"
               type="text"
               name="img_url"
               value={book.img_url}
               onChange={handleChange}
             />
-          </label>
-
-          <button className="submit-button">Submit</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-25">
+            <label htmlFor="subject">Description:</label>
+          </div>
+          <div className="col-75">
+            <textarea
+              name="description"
+              style={{ height: "200px" }}
+              value={book.description}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+        </div>
+        
+        <div className="row">
+          <input type="submit" value="Submit" />
+        </div>
       </form>
+    </div>
   );
 };
 export default BookCreate;
--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -43,3 +43,55 @@ function Login(props) {
   )
 }
 export default Login
+
+/* // <form className="create-form" onSubmit={handleSubmit}>
+
+      //     <label>
+      //       Title:
+      //       <input
+      //         className="input-title"
+      //         type="text"
+      //         name="title"
+      //         value={book.title}
+      //         onChange={handleChange}
+      //       />
+      //     </label>
+    
+  
+      //     <label>
+      //       Author:
+      //       <input
+      //         className="input-author"
+      //         type="text"
+      //         name="author"
+      //         value={book.author}
+      //         onChange={handleChange}
+      //       />
+      //     </label>
+
+      //     <label>
+      //       Description:
+      //       <textarea
+      //         className="textarea-description"
+      //         type="text"
+      //         rows={10}
+      //         cols={25}
+      //         name="description"
+      //         value={book.description}
+      //         onChange={handleChange}
+      //       />
+      //     </label>
+
+      //     <label>
+      //       Image Link:
+      //       <input
+      //         className="input-image-link"
+      //         type="text"
+      //         name="img_url"
+      //         value={book.img_url}
+      //         onChange={handleChange}
+      //       />
+      //     </label>
+
+      //     <button className="submit-button">Submit</button>
+      // </form>*/
