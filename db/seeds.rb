d1828eb81cc2724f4703eac6c3f72657b5b87cc6
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-28 19:49:12 -0400

    imported Book.css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+user1 = User.create({username: "Katie", password: "spot1201", password_confirmation: "spot1201"})
