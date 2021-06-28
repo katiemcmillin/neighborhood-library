d054a83cef0e391262c47114f124899f3f17f98a
Katie McMillin <katiemcmillin@Katies-MBP.home>
2021-06-28 08:35:33 -0400

    edited BookCreate.css

--- client/src/screens/BookCreate/BookCreate.jsx
+++ client/src/screens/BookCreate/BookCreate.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { postBook, getAllBooks } from "../../services/books";
 import { Redirect } from "react-router-dom"
+import './BookCreate.css'
 const BookCreate = (props) => {
   const [book, setBook] = useState({
     title: "",
@@ -28,38 +29,48 @@ const BookCreate = (props) => {
   if (isCreated) {
     return <Redirect to='/' />;
   }
-  return (<form onSubmit={handleSubmit}>
+  return (<form className="create-form" onSubmit={handleSubmit}>
+    <div className="input-container">
     <label>Title:
       <input
         type='text'
         name='title'
         value={book.title}
         onChange={handleChange} />
-    </label>
+      </label>
+    </div>
+    <div className="input-container">
     <label>Author:
       <input
         type='text'
         name='author'
         value={book.author}
         onChange={handleChange} />
-    </label>
+      </label>
+    </div>
+    <div className="input-container">
     <label>Description:
-      <input
+      <textarea
         type='text'
         rows={10}
-        cols={78}
+        cols={25}
         name='description'
         value={book.description}
         onChange={handleChange} />
-    </label>
+      </label>
+    </div>
+    <div className="input-container">
     <label>Image Link:
       <input
         type='text'
         name='img_url'
         value={book.img_url}
         onChange={handleChange} />
-    </label>
-    <button>Submit</button>
+      </label>
+    </div>
+    <div className="input-container">
+      <button>Submit</button>
+    </div>
   </form>)
 }
   export default BookCreate;
