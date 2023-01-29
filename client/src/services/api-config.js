dfc9803fd5008b801d051790c8dde9dd7a224d58
katiemcmillin <katiemcmillin@gmail.com>
2023-01-29 14:21:03 -0500

    changed deployment url and added seed command

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://neighborhood-library-api.herokuapp.com/"
+    ? "https://library-backend-s55f.onrender.com/"
     : "http://localhost:3000";
 
 const api = axios.create({
